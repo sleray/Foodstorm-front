427eaf4c4095737aefdac2324ec9ce5906610157
Jean-Philippe ETRILLARD <jphetr@gmail.com>
2018-03-22 23:51:42 +0100

    MAJ mdbootstrap

--- FoodStorm/src/app/app.component.ts
+++ FoodStorm/src/app/app.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-})
-@NgModule({
-  imports: [
-      MDBBootstrapModule.forRoot()
-  ],
-  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppComponent implements OnInit{
   title = 'Food Storm';
