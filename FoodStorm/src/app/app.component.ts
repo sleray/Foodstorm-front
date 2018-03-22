705622a182930d799f76922a5e32585b37e1f176
ETRILLARD <jean-philippe.etrillard@capgemini.com>
2018-03-22 17:57:07 +0100

    ajout de mdbootstrap / test foncitonnement dépendance

--- FoodStorm/src/app/app.component.ts
+++ FoodStorm/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+})
+@NgModule({
+  imports: [
+      MDBBootstrapModule.forRoot()
+  ],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppComponent implements OnInit{
   title = 'Food Storm';
